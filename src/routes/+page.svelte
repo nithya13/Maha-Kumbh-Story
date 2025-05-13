4c5a458f1489feab3fa9b54368a534ea877a9c2d
nithya13 <nithya13@gmail.com>
2025-05-13 14:53:43 +0530

    Map updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,10 @@
 
   let isOverlayVisible = false;
   let isPointsVisible = false;
+  let isNightVisible = false;
   let fadeTrigger;
   let pointsTrigger;
+  let nightTrigger;
 
   onMount(() => {
     const observer = new IntersectionObserver(
@@ -23,9 +25,15 @@
       },
       { threshold: 0.5 }
     );
-
+const observer3 = new IntersectionObserver(
+      ([entry]) => {
+        isNightVisible = entry.isIntersecting;
+      },
+      { threshold: 0.5 }
+    );
     if (fadeTrigger) observer.observe(fadeTrigger);
     if (pointsTrigger) observer2.observe(pointsTrigger);
+    if (nightTrigger) observer3.observe(nightTrigger);
   });
 </script>
 
@@ -107,7 +115,7 @@
   <section
     class="col-start-1 col-span-1 md:col-start-4 md:col-span-5 py-4 md:py-8 px-4 md:px-4 text-left"
   >
-  <h2 id="chapter1" class="text-2xl font-semibold mb-4">Chapter 1: Ecology, the River, and Rituals</h2>
+  <h2 id="chapter1" class="text-2xl font-semibold mb-4">Chapter 1: Rituals and River-Society Relationship</h2>
     <p class="text-base leading-relaxed">
       Prayagraj hosted more than 60 crore pilgrims and visitors over a span of
       45 days during the Maha Kumbh, according to the estimates of the Uttar
@@ -233,36 +241,44 @@
     <div class="sticky top-0 h-screen overflow-hidden z-10">
       <!-- Base Map -->
       <img
-        src={`${base}/Map2.png`}
+        src={`${base}/Basemap.png`}
         alt="Zoning"
-        class="w-full h-full object-contain"
+        class="w-full h-full object-cover"
       />
   
       <!-- First overlay -->
       <img
-        src={`${base}/overlay.png`}
+        src={`${base}/Fecal-coliform.png`}
         alt="Overlay"
-        class="absolute inset-0 w-full h-full object-contain transition-opacity duration-1200"
+        class="absolute inset-0 w-full h-full object-contain transition-opacity duration-800"
         class:opacity-0={!isOverlayVisible}
         class:opacity-100={isOverlayVisible}
       />
   
       <!-- Labels overlay -->
-      <img
-        src={`${base}/labels.png`}
+      <!-- <img
+        src={`${base}/Flower-tributes.png`}
         alt="Labels"
         class="absolute inset-0 w-full h-full object-contain transition-opacity duration-1000"
         class:opacity-0={!isOverlayVisible}
         class:opacity-100={isOverlayVisible}
-      />
+      /> -->
   
       <!-- Second overlay (points) -->
       <img
-        src={`${base}/points.png`}
+        src={`${base}/Flower-tributes.png`}
         alt="Points"
         class="absolute inset-0 w-full h-full object-contain transition-opacity duration-1000"
         class:opacity-0={!isPointsVisible}
         class:opacity-100={isPointsVisible}
+      />
+
+      <img
+        src={`${base}/Nightmap.jpg`}
+        alt="Points"
+        class="absolute inset-0 w-full h-full object-cover transition-opacity duration-100"
+        class:opacity-0={!isNightVisible}
+        class:opacity-100={isNightVisible}
       />
     </div>
   
@@ -277,33 +293,51 @@
       bind:this={pointsTrigger}
       class="absolute top-[320vh] h-[10vh] w-full"
     ></div>
+
+    <!-- Trigger area for night overlay -->
+    <div
+      bind:this={nightTrigger}
+      class="absolute top-[470vh] h-[10vh] w-full"
+    ></div>
   
     <!-- Floating cards area -->
     <div class="absolute top-[100vh] left-[10vw] w-full z-20">
       <div
         class="bg-white/80 backdrop-blur-lg rounded p-6 md:p-8 w-11/12 md:max-w-lg mr-3 mb-[100vh] shadow-lg"
       >
-        <h2 class="text-xl mb-2">Zoning</h2>
+        <h2 class="text-xl mb-2">Fecal coliform rose on snan days and then fell</h2>
         <p class="text-base leading-relaxed">
-          The area around the sangam is classified into zones for administrative purposes.
+          The Central Pollution Control Board’s (CPCB) monitoring during this Maha Kumbh as well as that in 2013 showed high levels of fecal coli, more so during the Amrit Snan (auspicious bathing) days. 
+There have been deliberate efforts to respond and mitigate this by allowing adequate flows in the river. As witnessed at the Kumbh of Prayag Raj, there has also been an upsurge of environmental 
+action to mitigate the impact, both by the state institutions and the civic society.
         </p>
       </div>
   
       <div
         class="bg-white/80 backdrop-blur-lg rounded p-6 md:p-8 w-11/12 md:max-w-lg mr-3 mb-[100vh] shadow-lg"
       >
-        <h2 class="text-xl mb-2">Rituals as Conservation</h2>
+        <h2 class="text-xl mb-2">Floral waste</h2>
         <p class="text-base leading-relaxed">
-          Events like the Maha Kumbh show that faith-based gatherings can support conservation goals if approached thoughtfully.
+          With all attempts at waste 
+management, visible piles of floral waste congregated along the banks, where sanitation 
+teams frequented discarded offerings for disposal. Significantly, vendors stood 
+strategically on both riverbanks and the Panda Pujaris (priests) to make it convenient for 
+pilgrims.
         </p>
       </div>
   
       <div
         class="bg-white/80 backdrop-blur-lg rounded p-6 md:p-8 w-11/12 md:max-w-lg mr-3 mb-[100vh] shadow-lg"
       >
-        <h2 class="text-xl mb-2">Power Structures</h2>
+        <h2 class="text-xl mb-2">Deep Daan</h2>
         <p class="text-base leading-relaxed">
-          The organization and functioning of the ephemeral city reflect deep social hierarchies, with power embedded in spatial and infrastructural choices.
+          During observations, it was seen that Deep Daan — offering lighted lamps to the river — 
+was one of the most commonly done rituals after the religious Snan or Aarti sessions. There 
+were several vendors positioned around the banks selling Deep Daan kits, usually 
+consisting of a small disposable dish containing flowers, vermilion (Kumkum/Sindoor), 
+incense sticks, and a lamp. Worshipers were spotted moving towards the riverbank 
+boundaries, especially by the floating interceptors that distinguish shallow banks from the 
+main channel, where they lit their lamps and let them float on the water. 
         </p>
       </div>
     </div>
