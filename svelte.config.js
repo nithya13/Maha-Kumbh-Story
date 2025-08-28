8fbbe68a5b0ff2c3985a85421bcc039fb2a9046a
nithya13 <nithya13@gmail.com>
2025-08-28 11:23:30 +0530

    config changed

--- svelte.config.js
+++ svelte.config.js
@@ -3,21 +3,10 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 
-const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: adapter({
-
-            fallback: 'index.html'
-
-        }),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Maha-Kumbh-Story' : '',
-        }
-    }
-    }
-
-export default config;
-
+export default {
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
+};
 
